1fdc2fd4ee7b393af0243ae5c598dfbb458a90d1
Mohamed Nuh <mojitsu185@gmail.com>
2020-06-23 00:27:34 -0700

    404

--- gallery/src/components/NotFound.js
+++ gallery/src/components/NotFound.js
@@ -1,12 +1,12 @@
-//import React
+
 import React from "react";
 
-//NotFound function when there was no result from Flickr
+
 const NotFound = () => (
     <li className="not-found">
-        <p>No Results Found</p>
-        <p>Your search did not return any results. Please try again :)</p>
+        <p>Nothing Found</p>
+        <p>You in the wrong neighbourhood cuhh </p>
     </li>
 );
-//export function
+
 export default NotFound;
