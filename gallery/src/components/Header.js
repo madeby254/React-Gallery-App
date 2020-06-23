cd585d70e409727978d06e17ff7183978d84d1d6
Mohamed Nuh <mojitsu185@gmail.com>
2020-06-23 00:27:18 -0700

    Header init

--- gallery/src/components/Header.js
+++ gallery/src/components/Header.js
@@ -4,7 +4,7 @@ import React from "react";
 //Header Function
 const Header = (props) => (
     <header>
-        <h1>Just Gallery App</h1>
+        <h1>Gallery App</h1>
     </header>
 );
 //export Header function
