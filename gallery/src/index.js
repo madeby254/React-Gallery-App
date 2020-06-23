dd1f3ce9a951ebc70e8a5d26a54e867af707e4d1
Mohamed Nuh <mojitsu185@gmail.com>
2020-06-23 01:42:05 -0700

    Refactored

--- gallery/src/index.js
+++ gallery/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 //import css
-import '../css/index.css';
+import './css/index.css';
 //import app component
 import App from './App';
 //import service worker
