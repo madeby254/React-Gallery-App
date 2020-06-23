21451398998358658d486513bb832800da1e1c81
Mohamed Nuh <mojitsu185@gmail.com>
2020-06-23 01:02:32 -0700

    Built Header

--- gallery/src/components/Header.js
+++ gallery/src/components/Header.js
@@ -1,11 +1,9 @@
-//import React
 import React from "react";
 
-//Header Function
 const Header = (props) => (
     <header>
-        <h1>Gallery App</h1>
+        <h1>React Gallery App</h1>
     </header>
 );
-//export Header function
+
 export default Header;
