1e03c34041c37962da03c30f31e23f65ca655f31
Mohamed Nuh <mojitsu185@gmail.com>
2020-06-23 01:42:09 -0700

    Refactored

--- gallery/src/components/Error.js
+++ gallery/src/components/Error.js
@@ -1,15 +1,14 @@
-//import React
 import React from "react";
 
-//Error function for displaying error when 404
+
 const Error = () => {
     return (
-    <div className="error">
-        <br/><br/>
-        <h1>Oh noooo! Something went wrong!</h1>
-        <p>It seems that the site you're requesting doesn't exist :(</p>
-        <p>You can search new value or for example choose cow.</p>
-    </div>
+        <div className="error">
+            <br /><br />
+            <h1>oops my bad something is wrong!</h1>
+            <p>i searched the Universe cant find what you need :(</p>
+
+        </div>
     )
 };
 
