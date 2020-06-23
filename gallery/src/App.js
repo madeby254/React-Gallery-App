5d8019b5c3a00631c00b5a22e2a86efe6a94f4a5
Mohamed Nuh <mojitsu185@gmail.com>
2020-06-23 01:41:55 -0700

    Not needed

--- gallery/src/App.js
+++ gallery/src/App.js
@@ -1,53 +1,48 @@
-//import React
 import React, { Component } from 'react';
-//import React Router elements
 import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
-//import axios for fetching data
 import axios from 'axios';
-//import components
 import Header from "./components/Header";
 import SearchForm from "./components/SearchForm";
 import Nav from "./components/Nav";
 import PhotoContainer from "./components/PhotoContainer";
 import Error from "./components/Error";
-//flicker config
 import apiKey from "./components/config";
 
 
-//app class handle page components
+
 class App extends Component {
     constructor() {
         super();
-        this.state = { //state is going to be a img data we want to display, a collection of objects
+        this.state = {
             images: [],
             isLoading: true
         };
     }
 
-    //Search Function
 
-    search = (query = 'horse') => {
+
+    search = (query = 'wildebeest') => {
         this.setState({
             isLoading: true,
             images: []
         });
 
         axios.get(` https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
-            .then(response => { //when response setState
+            .then(response => {
                 this.setState({
                     images: response.data.photos.photo,
                     isLoading: false,
                     query: query
                 });
             })
-            .catch(error => { //error while fetching data
+            .catch(error => {
                 console.log('Error fetching and parsing data', error);
             });
     };
 
-    //take care of rendering general look of the page, switch between routes
+
     render() {
-        // console.log(this.state.images);
+
         return (
             <Router>
                 <div className="container">
@@ -58,7 +53,7 @@ class App extends Component {
                     <Nav navSearch={this.search} />
 
                     <Switch>
-                        <Route exact path="/" render={() => <Redirect to="/search/horses" />} />
+                        <Route exact path="/" render={() => <Redirect to="/search/wildebeest" />} />
                         <Route path="/search/:query" render={({ match }) => (
                             <PhotoContainer
                                 routeMatch={match}
@@ -76,5 +71,5 @@ class App extends Component {
         );
     }
 }
-//export App class
+
 export default App;
